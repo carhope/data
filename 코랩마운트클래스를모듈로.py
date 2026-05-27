d7892fc1027f01fcea85821d6cc94a2c3cb28f50
carhope <26_61@bssm.hs.kr>
2026-05-27 09:45:35 +0900

    자구 비선형 코랩마운트 클래스를 모듈로

---
+++ 코랩마운트클래스를모듈로.py
@@ -0,0 +1,41 @@
+with open("/content/drive/MyDrive/Colab Notebooks/stack.py","w",encoding="utf-8")as f:
+  f.write("""
+class stack:
+  def __init__(self, size):
+    self.size=size
+    self.arr=[None]*self.size
+    self.top=-1
+  def isEmpty(self):
+    if self.top==-1:
+      print("스택이 공백 상태")
+      return 1;
+    else :
+      return 0;
+  def isFull(self):
+    if self.top == self.size-1:
+      print("스택이 포화상태")
+      return 1;
+    else :
+      return 0;
+  def push(self,data):
+    if self.isFull():
+      return
+    else :
+      self.top+=1
+      self.arr[self.top]=data
+  def pop(self):
+    if self.isEmpty():
+      return
+    else:
+      data=self.arr[self.top]
+      self.arr[self.top]=None
+      self.top-=1
+    return data
+""")
+  
+import sys
+sys.path.append('/content/drive/MyDrive/Colab Notebooks')
+from stack import stack
+s1 = stack(10)
+s1.push('aa')
+print(s1.pop())
